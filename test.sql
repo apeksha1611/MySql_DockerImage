2789fd6c4a257c0447b1a1914d745d16b5bd0742
apeksha1611 <80413750+apeksha1611@users.noreply.github.com>
2022-01-06 21:04:13 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE studentData(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Tanmay', 20112007, 'Bhubaneswar', 7978518314, 'SNRT213420');
+INSERT INTO studentData values('Lavdha', 20112008, 'Pune', 7978518321, 'ASDL200556');
+INSERT INTO studentData VALUES('Lassan', 20112009, 'MUMBAI', 1321212121, 'SRTL190587');
